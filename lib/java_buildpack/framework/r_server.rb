71836939c8b122e339ca3e4fab631d4450e0301e
nvanhen <pvanhengel@ofiglobal.com>
2016-06-23 14:07:42 -0400

    change to binary download

--- lib/java_buildpack/framework/r_server.rb
+++ lib/java_buildpack/framework/r_server.rb
@@ -38,7 +38,7 @@ module JavaBuildpack
         system 'wget http://cran.r-project.org/bin/linux/ubuntu/trusty/r-base-core_3.3.0-2trusty0_i386.deb'
 
         system 'mkdir r'
-        system 'dpkg-deb -x *.deb /r'
+        system 'dpkg-deb -x *.deb r'
 
         #system 'tar -xzf R-3.3.1.tar.gz'
         #system 'cd R-3.3.1'
