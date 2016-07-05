5d4adf0a190b8328848fa112137a14eb2603ebc3
nvanhen <pvanhengel@ofiglobal.com>
2016-07-05 13:40:53 -0400

    change to binary download

--- lib/java_buildpack/framework/r_server.rb
+++ lib/java_buildpack/framework/r_server.rb
@@ -31,13 +31,17 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
 
+        puts("TEST0: " + ARGV[0])
+
+        puts("TEST1: " + ARGV[1])
+
         #system 'wget http://cran.r-project.org/src/base/R-3/R-3.3.1.tar.gz'
 
         system 'wget http://cran.r-project.org/bin/linux/ubuntu/trusty/r-base-core_3.3.0-2trusty0_i386.deb'
         system 'mkdir r'
         system 'dpkg-deb -x *.deb r'
 
-        system 'ls -R -ltr'
+        # system 'ls -R -ltr'
 
         Dir.chdir 'r/usr/bin'
 
