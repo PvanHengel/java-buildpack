7309abe3fb0305ef0a2ae99c7dde3b47b5476513
nvanhen <pvanhengel@ofiglobal.com>
2016-07-08 11:18:15 -0400

    change to binary download

--- lib/java_buildpack/framework/r_server.rb
+++ lib/java_buildpack/framework/r_server.rb
@@ -59,8 +59,8 @@ module JavaBuildpack
       #  STDOUT.write 'PvH was here - release'
         #system '/home/vcap/app/vendor/ROUT/bin/R CMD Rserve'
         #puts("default_")
-        @droplet.environment_variables.add_environment_variable 'R_HOME', '/home/vcap/app/vendor/R'
-        @droplet.java_opts.add_system_property 'java.library.path', '/home/vcap/app/vendor/R'
+        @droplet.environment_variables.add_environment_variable 'R_HOME', '/home/vcap/app/vendor/ROUT/bin'
+        @droplet.java_opts.add_system_property 'java.library.path', '/home/vcap/app/vendor/ROUT/bin'
 
 
       end
